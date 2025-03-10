fe83355e67865f2bc422f350f4948dc5c8250446
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2025-03-10 23:51:18 +0530

    c str translation from c++ str for execvp

--- shell.cpp
+++ shell.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <cstdio>
-#include <string>
+#include <cstring>
 #include <unordered_map>
 #include <sstream>
 #include <functional>
+#include <thread>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 using namespace std;
-
-/*
-    Constants
-*/
-const string PROMPT = "> ";
-const int BUFFER_LEN = 1024;
-
-using func = function<int(vector<string>&)>;
-
-// mapping of built-in commands to their respective functions
-unordered_map<string, func> built_in_cmds = {
-    {"cd", cmd_cd},
-    {"help", cmd_help},
-    {"exit", cmd_exit}
-};
 
 ////////////////////////// Prototypes //////////////////////////
 // External commands
@@ -38,12 +27,76 @@ vector<string> parse_args(string& args);
 string read_line();
 void repl_loop();
 
+/*
+    Constants
+*/
+const string PROMPT = "> ";
+const int BUFFER_LEN = 1024;
+
+using func = function<int(vector<string>&)>;
+
+// mapping of built-in commands to their respective functions
+unordered_map<string, func> built_in_cmds = {
+    {"cd", cmd_cd},
+    {"help", cmd_help},
+    {"exit", cmd_exit}
+};
+
 ////////////////////////// Implementations //////////////////////////
 
 /*
     Command execution
 */
+void free_args(char**& args) {
+    for(int i = 0; i < sizeof(args) / sizeof(char*); i++)
+        free(args[i]);
+    free(args);
+}
+
 int launch_cmd(vector<string>& args) {
+    // launch the command in a child process
+    pid_t pid = fork();
+    char** c_args = (char**)malloc(sizeof(char*) * args.size() + 1);
+
+    int i = 0;
+    for(auto arg: args) {
+        c_args[i++] = strdup(arg.c_str());
+    }
+
+    // child process
+    if (pid == 0) {
+        if(execvp(args[0].c_str(), c_args) == -1) {
+            free_args(c_args);
+            perror("[shell]");
+            return 0;
+        }
+    }
+    // error forking
+    else if(pid < 0) {
+        cerr << "Error forking process: " <<  getpid() << endl;
+        free_args(c_args);
+        perror("[shell]");
+        return 0;
+    }
+    // parent process
+    else {
+        int status;
+        do {
+            // wait till the child is not stopped, when
+            // it does, return the status of the child
+            waitpid(pid, &status, WUNTRACED);
+        }
+        // the child process state can change multiple times
+        // during its execution and due to that it can be at "Stopped"
+        // state.
+        // WIFEXITED(status) returns true if the child terminated normally
+        // WIFSIGNALED(status) returns true if the child process was terminated by a signal
+        // So we continue only if the child process didnt exit or wasnt
+        // signalled to stop 
+        while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    
+    free_args(c_args);
     return 1;
 }
 
@@ -95,7 +148,7 @@ string read_line() {
         exit(EXIT_FAILURE);
     }
 
-    // Ctr + D
+    // Ctr + D at the end of some input
     if (cin.eof()) {
         cout << "EOF reached" << endl;
         exit(EXIT_SUCCESS);
@@ -130,7 +183,7 @@ void repl_loop() {
     }
 }
 
-int main(int argc, char argv[]) {
+int main(int argc, char** argv) {
     repl_loop();
     return 0;
 }
